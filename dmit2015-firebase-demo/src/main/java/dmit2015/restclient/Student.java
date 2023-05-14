e3dc0cc889f7131f0ee4b7f645d61373fbe45618
Amandeep Kaur Chahal <achahal12@nait.ca>
2023-05-13 19:03:32 -0600

    created project

---
+++ dmit2015-firebase-demo/src/main/java/dmit2015/restclient/Student.java
@@ -0,0 +1,33 @@
+package dmit2015.restclient;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter @Setter
+@NoArgsConstructor
+public class Student {
+//@Getter @Setter
+    private String firstName;
+
+ //   @Getter @Setter
+    private String lastName;
+
+//    public String getFirstName() {
+//        return firstName;
+//    }
+//
+//    public void setFirstName(String firstName) {
+//        this.firstName = firstName;
+//    }
+//
+//    public String getLastName() {
+//        return lastName;
+//    }
+//
+//    public void setLastName(String lastName) {
+//        this.lastName = lastName;
+//    }
+//
+//    public Student() {
+//    }
+}
